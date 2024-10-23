e37f74be7dc9739f22622afd860ea30ae8d43920
Kwon <kwonline21@gmail.com>
2024-10-23 19:39:59 +0900

    buffer

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
 import net from 'net';
+import { readHeader, writeHeader } from './utils.js';
+import { HANDLER_ID, TOTAL_LENGTH_SIZE } from './constants.js';
 
 const HOST = 'localhost';
 const PORT = 5555;
@@ -7,10 +9,25 @@ const client = new net.Socket();
 
 client.connect(PORT, HOST, () => {
   console.log('Connected to the server...');
+
+  const message = 'Hello';
+  const buffer = Buffer.from(message);
+
+  const header = writeHeader(buffer.length, 10);
+  const packet = Buffer.concat([header, buffer]);
+  client.write(packet);
 });
 
 client.on('data', (data) => {
-  console.log(data);
+  const buffer = Buffer.from(data);
+
+  const { length, handlerId } = readHeader(data);
+  console.log(`handlerId: ${handlerId}`);
+  console.log(`length: ${length}`);
+
+  const headerSize = TOTAL_LENGTH_SIZE + HANDLER_ID; // 6
+  const message = buffer.slice(headerSize);
+  console.log(`server 에게 받은 메세지: ${message}`);
 });
 
 client.on('close', () => {
